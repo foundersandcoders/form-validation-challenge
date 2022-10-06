a6e0cd1ddb89c06d343b8774fcd113699546b2e3
oliverjam <oliverjamesphillips@gmail.com>
2022-10-06 17:08:26 +0200

    Test challenge not solution!

--- test/helpers.js
+++ test/helpers.js
@@ -13,23 +13,7 @@ async function load(file) {
   });
 }
 
-const path = "solution/index.html";
+const path = "challenge/index.html";
 const loader = load(path);
-
-// function getByLabelText(document, text) {
-//   const labels = document.querySelectorAll("label");
-//   const label = [...labels].find(({ textContent }) => textContent === text);
-//   if (!label) {
-//     throw new Error(`Couldn't find label with textContent '${text}'`);
-//   }
-//   if (!label.htmlFor) {
-//     throw new Error(`Label \`${label.outerHTML}\` has no \`for\` attribute`);
-//   }
-//   const el = document.querySelector("#" + label.htmlFor);
-//   if (!el) {
-//     throw new Error(`Couldn't find element with id '${label.htmlFor}'`);
-//   }
-//   return el;
-// }
 
 module.exports = { loader };
